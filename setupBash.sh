3412c76c2a75a89ea7a8991630e8c11ccef3a2a9
manvelh <42197045+manvelh@users.noreply.github.com>
2020-03-23 17:28:58 -0500

    Update setupBash.sh

--- setupBash.sh
+++ setupBash.sh
@@ -7,7 +7,7 @@ sudo apt-get install gdal-bin
 sudo apt-get install python3-gdal 
 sudo apt-get install python3-numpy
 sudo apt-get install python3-pip
-pip3 install shapely=1.6.4
+pip3 install shapely==1.6.4
 pip3 install pyproj==1.9.6
 pip3 install geojson
 
